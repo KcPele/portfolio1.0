bfab94838ebc73797b62462503deabf681f919b9
Kc pele <fidekg123@gmail.com>
2022-09-08 21:20:57 +0100

    Update Footer.tsx

--- src/container/Footer/Footer.tsx
+++ src/container/Footer/Footer.tsx
@@ -54,7 +54,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:fidekg123@.com" className="p-text">fidekg123@gmail.com</a>
+          <a href="mailto:fidekg123@gmail.com" className="p-text">fidekg123@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
