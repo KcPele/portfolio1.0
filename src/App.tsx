03734a9a148a55ecfcad670ad4625e95a55ee343
kcpele <fidekg123@gmail.com>
2022-10-25 14:28:25 +0100

    image drag disabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { About, Header, Testimonial, Skills, Work, Footer } from './container';
 import { Navbar } from './components';
 const  App: React.FC = () => {
+//   useEffect(() => {
+//     document.getElementsByTagName("img").setAttribute('draggable', false);
+// }, [])
   return (
     <div className="app">
       <Navbar />
